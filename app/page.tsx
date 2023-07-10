f744b25eb0835b2c6a0a548ef3277896ed21286e
rush1dan <rushidan10@gmail.com>
2023-07-10 18:43:24 +0600

    Hero section init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/Header'
-import { useState, useEffect } from 'react'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
@@ -12,6 +12,9 @@ export default function Home() {
       <Header />
 
       {/* Hero */}
+      <section>
+        <Hero />
+      </section>
 
       {/* About */}
 

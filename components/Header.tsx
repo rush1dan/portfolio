f91aff3b4cb6aad2475ca08891b8d58ec7914db0
rush1dan <rushidan10@gmail.com>
2023-07-10 18:16:11 +0600

    Header WIP

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { SocialIcon } from 'react-social-icons'
 
 type Props = {}
 
 export default function Header({ }: Props) {
     return (
-        <header>I am a Header</header>
+        <header className=' sticky top-0 p-5 flex flex-row items-center justify-between max-w-7xl mx-auto z-20'>
+            <div className='flex flex-row items-center'>
+                <SocialIcon url='https://www.facebook.com' fgColor='gray' bgColor='transparent' />
+                <SocialIcon url='https://www.youtube.com' fgColor='gray' bgColor='transparent' />
+                <SocialIcon url='https://www.instagram.com' fgColor='gray' bgColor='transparent' />
+                <SocialIcon url='https://www.twitter.com' fgColor='gray' bgColor='transparent' />
+            </div>
+
+            <div className=' cursor-pointer flex flex-row items-center'>
+                <SocialIcon network='email' fgColor='gray' bgColor='transparent' />
+                <p className='uppercase text-sm text-gray-400 hidden md:inline-flex'>Get In Touch</p>
+            </div>
+        </header>
     )
 }

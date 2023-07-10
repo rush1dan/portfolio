82238eafc16fb54cf4b2f99c850f7a9aa35d64eb
rush1dan <rushidan10@gmail.com>
2023-07-10 18:38:32 +0600

    Framer motion animation on header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/Header'
-import Image from 'next/image'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
   return (
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,54 @@
+'use client'
+
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
+
 
 type Props = {}
 
 export default function Header({ }: Props) {
     return (
         <header className=' sticky top-0 p-5 flex flex-row items-center justify-between max-w-7xl mx-auto z-20'>
-            <div className='flex flex-row items-center'>
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className='flex flex-row items-center'>
                 <SocialIcon url='https://www.facebook.com' fgColor='gray' bgColor='transparent' />
                 <SocialIcon url='https://www.youtube.com' fgColor='gray' bgColor='transparent' />
                 <SocialIcon url='https://www.instagram.com' fgColor='gray' bgColor='transparent' />
                 <SocialIcon url='https://www.twitter.com' fgColor='gray' bgColor='transparent' />
-            </div>
+            </motion.div>
 
-            <div className=' cursor-pointer flex flex-row items-center'>
+            <motion.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className=' cursor-pointer flex flex-row items-center'>
                 <SocialIcon network='email' fgColor='gray' bgColor='transparent' />
                 <p className='uppercase text-sm text-gray-400 hidden md:inline-flex'>Get In Touch</p>
-            </div>
+            </motion.div>
         </header>
     )
 }

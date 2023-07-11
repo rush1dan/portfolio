d9e747c2fde4f57ff9eb62bbf4e442eaf24681af
rush1dan <rushidan10@gmail.com>
2023-07-11 15:14:46 +0600

    Typewriter effect implemented

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,23 @@
+'use client'
+
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 type Props = {}
 
 export default function Hero({ }: Props) {
+    const [text, count] = useTypewriter({
+        words: ["a Game Developer.", "a Software Engineer.", "an avid learner."],
+        loop: true,
+        delaySpeed: 2000
+    })
     return (
-        <div>Hero</div>
+        <div>
+            <h1 className=' text-2xl'>
+                <span>Hi, I'm <span className=' font-semibold'>Rushidan</span>, </span>
+                <span className=' font-bold'>{text}</span>
+                <Cursor cursorBlinking={true} cursorColor='#777777' />
+            </h1>
+        </div>
     )
 }

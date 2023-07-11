609259784e47446314e40145298b6ffbce45511d
rush1dan <rushidan10@gmail.com>
2023-07-11 21:04:38 +0600

    Hero section wip

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
@@ -7,17 +8,28 @@ type Props = {}
 
 export default function Hero({ }: Props) {
     const [text, count] = useTypewriter({
-        words: ["a Game Developer.", "a Software Engineer.", "an avid learner."],
+        words: ["A Game Developer.", "A Software Engineer.", "An avid learner."],
         loop: true,
         delaySpeed: 2000
     })
     return (
         <div>
-            <h1 className=' text-2xl'>
-                <span>Hi, I'm <span className=' font-semibold'>Rushidan</span>, </span>
-                <span className=' font-bold'>{text}</span>
-                <Cursor cursorBlinking={true} cursorColor='#777777' />
-            </h1>
+            <div className=' flex flex-col-reverse items-center justify-center space-y-10 
+            md:flex-row md:space-x-16'>
+                <div className='flex flex-col items-center justify-center space-y-2 md:w-64'>
+                    <h1 className=' text-2xl'>
+                        <span>Hi, I'm <span className=' font-semibold'>Rushidan</span>, </span>
+                    </h1>
+                    <div>
+                        <h2 className=' inline-block font-bold text-2xl'>{text}</h2>
+                        <Cursor cursorBlinking={true} cursorColor='#777777' />
+                    </div>
+                </div>
+                <div></div>
+                <div>
+                    <Image src={'/dp.png'} alt='profile picture' width={256} height={256} priority />
+                </div>
+            </div>
         </div>
     )
 }

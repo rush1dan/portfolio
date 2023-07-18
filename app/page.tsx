62517726726e4f995866257ab70106a8403535e9
rush1dan <rushidan10@gmail.com>
2023-07-18 19:44:44 +0600

    Skills page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from '@/components/About'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import Skills from '@/components/Skills'
 
 export default function Home() {
   return (
@@ -23,9 +24,10 @@ export default function Home() {
         <About />
       </section>
 
-      {/* Experience */}
-
-      {/* Skill */}
+      {/* Skills */}
+      <section id="skills">
+        <Skills />
+      </section>
 
       {/* Projects */}
 

0fde79029c0cea2d7bbcbd7a131f2a07864553df
rush1dan <rushidan10@gmail.com>
2023-07-18 19:38:02 +0600

    About page animations complete for pc screen

--- components/About.tsx
+++ components/About.tsx
@@ -10,11 +10,11 @@ export default function About({ }: Props) {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true });
 
-    const slideInControls = useAnimation();
+    const revealAnimationControls = useAnimation();
     
     useEffect(() => {
         if (isInView) {
-            slideInControls.start("visible");
+            revealAnimationControls.start("visible");
         }
     }, [isInView]);
 
@@ -26,8 +26,40 @@ export default function About({ }: Props) {
                 
                 {/* About Section */}
                 <section className='flex flex-col items-center justify-center'>
-                    <h1 className='text-3xl font-bold text-gray-600'>About</h1>
-                    <p className='text-sm px-12'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                    
+                    {/* Heading */}
+                    <motion.h1 className='text-3xl font-bold text-gray-600'
+                        variants={{
+                            hidden: {
+                                y: -200,
+                                opacity: 0,
+                            },
+                            visible: {
+                                y: 0,
+                                opacity: 1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={revealAnimationControls}
+                        transition={{duration: 0.75}}>
+                        About
+                    </motion.h1>
+
+                    {/* Description */}
+                    <motion.p className='text-sm pt-4 px-12'
+                        variants={{
+                            hidden: {
+                                opacity: 0,
+                            },
+                            visible: {
+                                opacity: 1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={revealAnimationControls}
+                        transition={{duration: 0.75, delay: 0.75}}>
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    </motion.p>
                 </section>
 
                 {/* Experience/Education Headings Section*/}
@@ -46,7 +78,7 @@ export default function About({ }: Props) {
                             }
                         }}
                         initial="hidden"
-                        animate={slideInControls}
+                        animate={revealAnimationControls}
                         transition={{duration: 0.75}}>
                         <h1 className='text-3xl font-bold text-gray-600'>Education</h1>
                     </motion.div>
@@ -64,37 +96,135 @@ export default function About({ }: Props) {
                             }
                         }}
                         initial="hidden"
-                        animate={slideInControls}
+                        animate={revealAnimationControls}
                         transition={{duration: 0.75}}>
                         <h1 className='text-3xl font-bold text-gray-600'>Experience</h1>
                     </motion.div>
                 </div>
                 
                 {/* Education/Experience Description Section */}
-                <div className='bg-gray-600 w-1 h-[48rem] top-0 flex flex-col justify-around'>
-                    <div className='relative bg-gray-600 w-12 h-1'>
-                        <div className='absolute left-16 h-12 -top-8 w-96'>
+                
+                {/* Middle Line */}
+                <motion.div className='bg-gray-600 w-1 h-[48rem] origin-top flex flex-col justify-around'
+                    variants={{
+                        hidden: {
+                            scaleY: 0,
+                        },
+                        visible: {
+                            scaleY: 1,
+                        }
+                    }}
+                    initial="hidden"
+                    animate={revealAnimationControls}
+                    transition={{duration: 0.5, delay: 0.5}}>
+                    
+                    {/* Experience Top */}
+                    <motion.div className='relative bg-gray-600 w-12 h-1 origin-left'
+                        variants={{
+                            hidden: {
+                                scaleX: 0,
+                            },
+                            visible: {
+                                scaleX: 1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={revealAnimationControls}
+                        transition={{duration: 0.25, delay: 1}}>
+                        
+                        {/* Description */}
+                        <motion.div className='absolute left-16 h-12 -top-8 w-96'
+                            variants={{
+                                hidden: {
+                                    x: 300,
+                                    opacity: 0
+                                },
+                                visible: {
+                                    x: 0,
+                                    opacity: 1
+                                }
+                            }}
+                            initial="hidden"
+                            animate={revealAnimationControls}
+                            transition={{duration: 0.5, delay: 1.25}}>
                             <p>Game Programmer</p>
                             <p>Kolpoverse Studio</p>
                             <p>November, 2022 - March, 2023</p>
-                        </div>
-                    </div>
-                    <div className='relative bg-gray-600 w-12 h-1 -scale-x-100 -translate-x-12'>
-                        <div className='absolute left-16 h-12 -top-12 w-[26rem] -scale-x-100 text-right'>
+                        </motion.div>
+                    </motion.div>
+
+                    {/* Education Middle */}
+                    <motion.div className='relative bg-gray-600 w-12 h-1 -scale-x-100 -translate-x-12 origin-left'
+                        variants={{
+                            hidden: {
+                                scaleX: 0,
+                            },
+                            visible: {
+                                scaleX: -1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={revealAnimationControls}
+                        transition={{duration: 0.25, delay: 1}}>
+                        
+                        {/* Description */}
+                        <motion.div className='absolute left-16 h-12 -top-12 w-[26rem] -scale-x-100 text-right'
+                            variants={{
+                                hidden: {
+                                    x: 300,
+                                    opacity: 0,
+                                    scaleX: -1,
+                                },
+                                visible: {
+                                    x: 0,
+                                    opacity: 1
+                                }
+                            }}
+                            initial="hidden"
+                            animate={revealAnimationControls}
+                            transition={{duration: 0.5, delay: 1.25}}>
                             <p>BSc in Mechanical Engineering,</p>
                             <p>Bangladesh University of Engineering and Technology</p>
                             <p>CGPA 3.45 out of 4.00</p>
                             <p>Attended: 2016-2021</p>
-                        </div>
-                    </div>
-                    <div className='relative bg-gray-600 w-12 h-1'>
-                        <div className='absolute left-16 h-12 -top-8 w-96'>
+                        </motion.div>
+                    </motion.div>
+
+                    {/* Experience Bottom */}
+                    <motion.div className='relative bg-gray-600 w-12 h-1 origin-left'
+                        variants={{
+                            hidden: {
+                                scaleX: 0,
+                            },
+                            visible: {
+                                scaleX: 1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={revealAnimationControls}
+                        transition={{duration: 0.25, delay: 1}}>
+                        
+                        {/* Description */}
+                        <motion.div className='absolute left-16 h-12 -top-8 w-96'
+                            variants={{
+                                hidden: {
+                                    x: 300,
+                                    opacity: 0
+                                },
+                                visible: {
+                                    x: 0,
+                                    opacity: 1
+                                }
+                            }}
+                            initial="hidden"
+                            animate={revealAnimationControls}
+                            transition={{duration: 0.5, delay: 1.25}}>
                             <p>Game Programmer</p>
                             <p>Alpha Potato</p>
                             <p>March, 2020 - November, 2022</p>
-                        </div>
-                    </div>
-                </div>
+                        </motion.div>
+                    </motion.div>
+                </motion.div>
             </div>
         </div>
     )

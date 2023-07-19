30f75be25b65787af320de264898f4e97e696fa3
rush1dan <rushidan10@gmail.com>
2023-07-19 17:21:47 +0600

    Lottie animation playing

--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -1,11 +1,22 @@
+'use client'
+
+import { Player, Controls } from '@lottiefiles/react-lottie-player'
 import React from 'react'
 
 type Props = {}
 
-export default function Skills({}: Props) {
+export default function Skills({ }: Props) {
   return (
     <div className='h-screen snap-center bg-gradient-radial from-[rgb(54,54,54)] from-5% to-[rgb(36,36,36)] to-70%'>
-        
+      {/* Lottie animation */}
+      <div className='h-full flex flex-col items-center justify-center'>     
+        <Player
+          autoplay
+          loop
+          src="developer_lottie.json"
+          style={{ height: "300px", width: "300px" }} 
+        ></Player>
+      </div>
     </div>
   )
 }

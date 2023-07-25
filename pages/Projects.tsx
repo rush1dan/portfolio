abe2ee033a3010e3db67871c29bde522b75b0481
rush1dan <rushidan10@gmail.com>
2023-07-26 00:20:29 +0600

    Project page WIP

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import HeadingLine from '@/components/HeadingLine'
 
 type Props = {}
 
 export default function Projects({ }: Props) {
     return (
-        <div className='h-screen snap-center'>Projects</div>
+        <div className='h-screen snap-center'>
+            <div className="h-[13%]"></div>
+            <div className="flex flex-col items-center justify-start spacing-y-12 h-[87%]">
+                <div className="h-1/2 w-full">
+                    <HeadingLine
+                        heading='PROJECTS'
+                        marginPadding=''
+                        lineGap={4}
+                        lineWidth='w-[10vw]'
+                    />
+                </div>
+                <div className="h-1/2 w-full">
+                    <HeadingLine
+                        heading='GAMES'
+                        marginPadding=''
+                        lineGap={4}
+                        lineWidth='w-[10vw]'
+                    />
+                </div>
+            </div>
+        </div>
     )
 }

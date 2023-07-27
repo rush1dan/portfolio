d26c412ab43e133fb7f1f79421256c56fd566eca
rush1dan <rushidan10@gmail.com>
2023-07-27 23:58:04 +0600

    Project card almost finished

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,19 +15,25 @@ type ProjectCardProps = {
 export default function ProjectCard({ image, title, description, techStackUsed, gitHubURL, demoURL }: ProjectCardProps) {
     return (
         <div>
+            {/* Main Card */}
             <div className="bg-[rgb(46,46,46)] w-[16rem] h-[22rem] rounded-lg bg-clip-broder shadow-lg shadow-[rgb(36,36,36,0.5]">
+                {/* Image Container */}
                 <div className="h-[35%] p-4">
                     <div className={`h-full w-full rounded-lg ${styles.projectImage}`} style={{backgroundImage: `url('${image}')`}}></div>
                 </div>
+                {/* Info Container */}
                 <div className="h-[65%] relative">
                     <div className="absolute w-[50%] h-[2px] bg-slate-200 left-1/2 -translate-x-1/2"></div>
                     <div className="absolute w-full h-full p-4">
+                        {/* Project Title */}
                         <p className="text-lg font-semibold">{title}</p>
-                        <div className="w-full h-12 mt-2 bg-red-300">
+                        {/* Project Description */}
+                        <div className="w-full h-12 mt-2">
                             <p className='text-xs'>{description}</p>
                         </div>
-                        <div className='mt-4 w-full flex flex-row items-center justify-start space-x-2'>
-                            <p className='text-xs'>
+                        {/* Tech Stack */}
+                        <div className='mt-6 w-full flex flex-row items-center justify-start space-x-2'>
+                            <p className='text-xs font-semibold'>
                                 Tech:
                             </p>
                             {
@@ -47,6 +53,11 @@ export default function ProjectCard({ image, title, description, techStackUsed, 
                                 })
                             }
                         </div>
+                        {/* Project Links */}
+                        <div className="mt-6 w-full flex flex-row items-center justify-center space-x-8">
+                            <ProjectLinkButton iconSrc='icons/github-white.svg' title='Code'/>
+                            {demoURL && <ProjectLinkButton iconSrc='icons/link-white.svg' title='Demo' />}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -54,18 +65,22 @@ export default function ProjectCard({ image, title, description, techStackUsed, 
     )
 }
 
-type TechStackIconProps = {}
-
-function TechStackIcon({ }: TechStackIconProps) {
-    return (
-        <div>Icon</div>
-    )
+type ProjectLinkButtonProps = {
+    iconSrc: string,
+    title: string
 }
 
-type ProjectLinkButtonProps = {}
-
-function ProjectLinkButton({ }: ProjectLinkButtonProps) {
+function ProjectLinkButton({ iconSrc, title }: ProjectLinkButtonProps) {
     return (
-        <div>Button</div>
+        <div className='bg-stone-600 w-20 h-7 rounded-[0.175rem] border-stone-500 border-b-2 shadow-md shadow-[rgb(60,60,60,0.5)]'>
+            <div className="h-full flex flex-row items-center justify-center space-x-2">
+                <div className="w-[1.25rem] h-[1.25rem]">
+                    <img src={iconSrc} alt={title} />
+                </div>
+                <p>
+                    {title}
+                </p>
+            </div>
+        </div>
     )
 }

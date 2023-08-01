dc3787af6069becd4f12a2fae863933210988954
rush1dan <rushidan10@gmail.com>
2023-08-01 22:05:59 +0600

    Headling lines on Projects page animated on view

--- components/HeadingLine.tsx
+++ components/HeadingLine.tsx
@@ -15,12 +15,25 @@ export default function HeadingLine({ animationControls, heading, marginPadding,
         <div className="relative">
             <div className={`absolute w-full ${marginPadding}`}>
                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <div className={`absolute origin-left -translate-x-4 -scale-x-100 h-[1px] bg-slate-300 ${lineWidth} inline-block top-1/2 -translate-y-1/2`}>
-                    </div>
-                    <motion.p className="text-3xl text-gray-600 font-semibold inline"
-                         variants={{
+                    <motion.div className={`absolute origin-left -translate-x-4 -scale-x-100 h-[1px] bg-slate-300 ${lineWidth} 
+                        inline-block top-1/2 -translate-y-1/2`}
+                        variants={{
                             hidden: {
-                                opacity: 0,
+                                scaleX: 0,
+                                translateX: "-1rem",
+                            },
+                            visible: {
+                                scaleX: -1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={animationControls}
+                        transition={{ duration: 0.5, ease: 'easeInOut' }} >
+                    </motion.div>
+                    <motion.p className="text-3xl text-gray-600 font-semibold inline"
+                        variants={{
+                            hidden: {
+                                opacity: 0.5,
                             },
                             visible: {
                                 opacity: 1,
@@ -28,11 +41,23 @@ export default function HeadingLine({ animationControls, heading, marginPadding,
                         }}
                         initial="hidden"
                         animate={animationControls}
-                        transition={{ duration: 3 }} >
+                        transition={{ duration: 0.5 }} >
                         {heading}
                     </motion.p>
-                    <div className={`absolute origin-left translate-x-4 h-[1px] bg-slate-300 ${lineWidth} inline-block top-1/2 -translate-y-1/2`}>
-                    </div>
+                    <motion.div className={`absolute origin-left translate-x-4 h-[1px] bg-slate-300 ${lineWidth} inline-block top-1/2 -translate-y-1/2`}
+                        variants={{
+                            hidden: {
+                                scaleX: 0,
+                                translateX: "1rem"
+                            },
+                            visible: {
+                                scaleX: 1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={animationControls}
+                        transition={{ duration: 0.5, ease: 'easeInOut' }} >
+                    </motion.div>
                 </div>
             </div>
         </div>

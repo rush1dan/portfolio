8d06f74c274038c5c7150abfc646aa13c0c203c8
rush1dan <rushidan10@gmail.com>
2023-08-01 22:44:32 +0600

    Heading line animated on view on Skills page

--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -4,43 +4,52 @@ import { Player, } from '@lottiefiles/react-lottie-player'
 import React from 'react'
 import SkillCategory from '@/components/SkillCategory'
 import HeadingLine from '@/components/HeadingLine'
+import AnimateOnView, { AnimateableComponentProps } from '@/components/AnimateOnView'
 
 type Props = {}
 
 export default function Skills({ }: Props) {
-  // [iconSrc, iconAltText, hoverIconSrc, hoverIconAltText, hoverTitle]
+    // [iconSrc, iconAltText, hoverIconSrc, hoverIconAltText, hoverTitle]
 
-  const frontEndSkillList = ["html", "css", "tailwindcss", "javascript", "typescript", "react", "redux", "flutter"]
-  const backEndSkillList = ["nodejs", "expressjs", "mongodb", "dotnetcore", "microsoft-sql-server", "nextjs"]
-  const gameDevSkillList = ["unity", "blender"]
-  const languageSkillList = ["cpp", "csharp", "python"]
+    const frontEndSkillList = ["html", "css", "tailwindcss", "javascript", "typescript", "react", "redux", "flutter"]
+    const backEndSkillList = ["nodejs", "expressjs", "mongodb", "dotnetcore", "microsoft-sql-server", "nextjs"]
+    const gameDevSkillList = ["unity", "blender"]
+    const languageSkillList = ["cpp", "csharp", "python"]
 
-  return (
-    <div className='h-screen snap-center bg-gradient-radial from-[rgb(54,54,54)] from-5% to-[rgb(36,36,36)] to-70%'>
-      <HeadingLine 
-        heading='SKILLS'
-        marginPadding='mt-32'
-        lineWidth='w-[30vw]'
-      />
-      {/* Main Flex Container */}
-      <div className='h-full flex flex-row items-center justify-center space-x-40'>
-        
-        {/* Lottie animation */}
-        <Player
-          autoplay
-          loop
-          src="developer_lottie.json"
-          style={{ height: "300px", width: "300px" }}
-        ></Player>
-        
-        {/* Skill flex container */}
-        <div className='flex flex-col space-y-24'>
-          <SkillCategory category='Front End' skillList={frontEndSkillList} />
-          <SkillCategory category='Back End' skillList={backEndSkillList} />
-          <SkillCategory category='Game Dev' skillList={gameDevSkillList} />
-          <SkillCategory category='Languages' skillList={languageSkillList} />
+    return (
+        <AnimateOnView Component={SkillsPageContent} otherProps={{ frontEndSkillList, backEndSkillList, gameDevSkillList, languageSkillList }} />
+    )
+}
+
+function SkillsPageContent({ animationControls, otherProps }: AnimateableComponentProps)
+{
+    return (
+        <div className='h-screen snap-center bg-gradient-radial from-[rgb(54,54,54)] from-5% to-[rgb(36,36,36)] to-70%'>
+            <HeadingLine
+                animationControls={animationControls}
+                heading='SKILLS'
+                marginPadding='mt-32'
+                lineWidth='w-[30vw]'
+            />
+            {/* Main Flex Container */}
+            <div className='h-full flex flex-row items-center justify-center space-x-40'>
+
+                {/* Lottie animation */}
+                <Player
+                    autoplay
+                    loop
+                    src="developer_lottie.json"
+                    style={{ height: "300px", width: "300px" }}
+                ></Player>
+
+                {/* Skill flex container */}
+                <div className='flex flex-col space-y-24'>
+                    <SkillCategory category='Front End' skillList={otherProps.frontEndSkillList} />
+                    <SkillCategory category='Back End' skillList={otherProps.backEndSkillList} />
+                    <SkillCategory category='Game Dev' skillList={otherProps.gameDevSkillList} />
+                    <SkillCategory category='Languages' skillList={otherProps.languageSkillList} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }

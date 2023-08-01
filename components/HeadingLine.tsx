73f0549550b66d1bd389cd74105f0d33ad57b353
rush1dan <rushidan10@gmail.com>
2023-08-01 21:46:29 +0600

    Heading line wrapped with Animate On View

--- components/HeadingLine.tsx
+++ components/HeadingLine.tsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import { AnimateableComponentProps } from './AnimateOnView'
+'use client'
 
-type HeadingLineProps = AnimateableComponentProps & {
+import React from 'react'
+import AnimateOnView, { AnimateableComponentProps } from './AnimateOnView'
+import { motion } from 'framer-motion'
+
+type HeadingLineProps = {
     heading: string,
     marginPadding: string,
     lineWidth: string | number
 }
 
-export default function HeadingLine({ animationControls, heading, marginPadding, lineWidth }: HeadingLineProps) {
+export default function HeadingLine({ heading, marginPadding, lineWidth }: HeadingLineProps) {
+    return (
+        <AnimateOnView Component={HeadingLineContent} otherProps={{ heading, marginPadding, lineWidth }} />
+    )
+}
+
+function HeadingLineContent({ animationControls, otherProps  }: AnimateableComponentProps)
+{
     return (
         <div className="relative">
-            <div className={`absolute w-full ${marginPadding}`}>
+            <div className={`absolute w-full ${otherProps.marginPadding}`}>
                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                    <div className={`absolute origin-left -translate-x-4 -scale-x-100 h-[1px] bg-slate-300 ${lineWidth} inline-block top-1/2 -translate-y-1/2`}>
+                    <div className={`absolute origin-left -translate-x-4 -scale-x-100 h-[1px] bg-slate-300 ${otherProps.lineWidth} inline-block top-1/2 -translate-y-1/2`}>
                     </div>
-                    <p className="text-3xl text-gray-600 font-semibold inline">{heading}</p>
-                    <div className={`absolute origin-left translate-x-4 h-[1px] bg-slate-300 ${lineWidth} inline-block top-1/2 -translate-y-1/2`}>
+                    <motion.p className="text-3xl text-gray-600 font-semibold inline"
+                         variants={{
+                            hidden: {
+                                opacity: 0,
+                            },
+                            visible: {
+                                opacity: 1,
+                            }
+                        }}
+                        initial="hidden"
+                        animate={animationControls}
+                        transition={{ duration: 3 }} >
+                        {otherProps.heading}
+                    </motion.p>
+                    <div className={`absolute origin-left translate-x-4 h-[1px] bg-slate-300 ${otherProps.lineWidth} inline-block top-1/2 -translate-y-1/2`}>
                     </div>
                 </div>
             </div>

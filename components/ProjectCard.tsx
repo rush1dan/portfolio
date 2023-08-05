8c34d0e64a1e1031f0fcc8ce2e9c4946d4972226
rush1dan <rushidan10@gmail.com>
2023-08-06 00:18:15 +0600

    Styling moved in dedicated folder

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './projectcardstyle.module.css'
+import styles from '../styles/projectcardstyle.module.css'
 import Tech from './Tech'
 import { techStack } from '@/myutils/techStack'
 

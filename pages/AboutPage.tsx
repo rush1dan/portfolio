352852b70cd6b7b9de9782c59a46b246648e6d91
rush1dan <rushidan10@gmail.com>
2023-08-07 20:02:36 +0600

    Better layout for about page

--- pages/AboutPage.tsx
+++ pages/AboutPage.tsx
@@ -16,8 +16,9 @@ export default function AboutPage({ }: Props) {
 function AboutPageContent({ animationControls }: AnimateableComponentProps) {
     return (
         <div className='h-screen snap-center'>
+            <div className="h-24"></div>
             {/* Main Flex Container */}
-            <div className='h-full p-24 flex flex-col items-center justify-start space-y-8'>
+            <div className='h-full flex flex-col items-center justify-start space-y-8'>
 
                 {/* About Section */}
                 <section className='flex flex-col items-center justify-center'>
@@ -27,11 +28,11 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                         animationControls={animationControls}
                         heading='ABOUT'
                         marginPadding=''
-                        lineWidth='w-[30vw]'
+                        lineWidth='w-[10vw]'
                     />
 
                     {/* Description */}
-                    <motion.p className='text-sm pt-8 px-12'
+                    <motion.p className='pt-8 px-4 max-w-5xl font-medium text-sm md:text-base text-justify'
                         variants={{
                             hidden: {
                                 opacity: 0,
@@ -43,12 +44,15 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                         initial="hidden"
                         animate={animationControls}
                         transition={{ duration: 0.75, delay: 0.75 }}>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                        I'm Rushidan Islam, a former game developer who is recently venturing into the world of fullstack software development.
+                        I'm always enthusiastic about learning new technologies and overcoming the challenges that come as result.
+                        Having skill and experience in a wide range of software development areas such as game development, desktop applications and web development,
+                        allows me to analyze problems from multiple directions and come up with unique and effective solutions.
                     </motion.p>
                 </section>
 
                 {/* Experience/Education Headings Section*/}
-                <div className='w-full flex flex-col items-center justify-around md:flex-row'>
+                <div className='pt-8 w-full flex items-center justify-center space-x-[40rem] flex-row'>
 
                     {/* Education Heading */}
                     <HeadingLine
@@ -70,7 +74,7 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                 {/* Education/Experience Description Section */}
 
                 {/* Middle Line */}
-                <motion.div className='bg-headingColor w-1 h-[48rem] origin-top flex flex-col justify-around'
+                <motion.div className='bg-headingColor w-1 h-[36rem] origin-top flex flex-col justify-around'
                     variants={{
                         hidden: {
                             scaleY: 0,
@@ -84,7 +88,7 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                     transition={{ duration: 0.5, delay: 0.5 }}>
 
                     {/* Experience Top */}
-                    <motion.div className='relative bg-headingColor w-12 h-1 origin-left'
+                    <motion.div className='relative bg-headingColor w-24 h-1 origin-left'
                         variants={{
                             hidden: {
                                 scaleX: 0,
@@ -98,7 +102,7 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                         transition={{ duration: 0.25, delay: 1 }}>
 
                         {/* Description */}
-                        <motion.div className='absolute left-16 h-12 -top-8 w-96'
+                        <motion.div className='absolute left-32 h-12 -top-14 w-96'
                             variants={{
                                 hidden: {
                                     x: 300,
@@ -112,14 +116,16 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                             initial="hidden"
                             animate={animationControls}
                             transition={{ duration: 0.5, delay: 1.25 }}>
-                            <p>Game Programmer</p>
-                            <p>Kolpoverse Studio</p>
-                            <p>November, 2022 - March, 2023</p>
+                            <div className='p-4 bg-zinc-700 rounded-lg border-2 border-zinc-400 shadow-stone-900 shadow-lg'>
+                                <p className='text-lg font-medium'>Game Programmer</p>
+                                <p className='text-lg font-medium'>Kolpoverse Studio</p>
+                                <p className='text-lg font-medium'>November, 2022 - March, 2023</p>
+                            </div>
                         </motion.div>
                     </motion.div>
 
                     {/* Education Middle */}
-                    <motion.div className='relative bg-headingColor w-12 h-1 -scale-x-100 -translate-x-12 origin-left'
+                    <motion.div className='relative bg-headingColor w-24 h-1 -scale-x-100 -translate-x-12 origin-left'
                         variants={{
                             hidden: {
                                 scaleX: 0,
@@ -133,7 +139,7 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                         transition={{ duration: 0.25, delay: 1 }}>
 
                         {/* Description */}
-                        <motion.div className='absolute left-16 h-12 -top-12 w-[26rem] -scale-x-100 text-right'
+                        <motion.div className='absolute left-32 h-12 -top-[4.5rem] w-[32rem] -scale-x-100 text-right'
                             variants={{
                                 hidden: {
                                     x: 300,
@@ -148,15 +154,17 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                             initial="hidden"
                             animate={animationControls}
                             transition={{ duration: 0.5, delay: 1.25 }}>
-                            <p>BSc in Mechanical Engineering,</p>
-                            <p>Bangladesh University of Engineering and Technology</p>
-                            <p>CGPA 3.45 out of 4.00</p>
-                            <p>Attended: 2016-2021</p>
+                            <div className='p-4 bg-zinc-700 rounded-lg border-2 border-zinc-400 shadow-stone-900 shadow-lg'>
+                                <p className='text-base font-medium'>BSc in Mechanical Engineering,</p>
+                                <p className='text-base font-medium'>Bangladesh University of Engineering and Technology</p>
+                                <p className='text-base font-medium'>CGPA 3.45 out of 4.00</p>
+                                <p className='text-base font-medium'>Attended: 2016-2021</p>
+                            </div>
                         </motion.div>
                     </motion.div>
 
                     {/* Experience Bottom */}
-                    <motion.div className='relative bg-headingColor w-12 h-1 origin-left'
+                    <motion.div className='relative bg-headingColor w-24 h-1 origin-left'
                         variants={{
                             hidden: {
                                 scaleX: 0,
@@ -170,7 +178,7 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                         transition={{ duration: 0.25, delay: 1 }}>
 
                         {/* Description */}
-                        <motion.div className='absolute left-16 h-12 -top-8 w-96'
+                        <motion.div className='absolute left-32 h-12 -top-14 w-96'
                             variants={{
                                 hidden: {
                                     x: 300,
@@ -184,9 +192,11 @@ function AboutPageContent({ animationControls }: AnimateableComponentProps) {
                             initial="hidden"
                             animate={animationControls}
                             transition={{ duration: 0.5, delay: 1.25 }}>
-                            <p>Game Programmer</p>
-                            <p>Alpha Potato</p>
-                            <p>March, 2020 - November, 2022</p>
+                            <div className='p-4 bg-zinc-700 rounded-lg border-2 border-zinc-400 shadow-stone-900 shadow-lg'>
+                                <p className='text-lg font-medium'>Game Programmer</p>
+                                <p className='text-lg font-medium'>Alpha Potato</p>
+                                <p className='text-lg font-medium'>March, 2020 - November, 2022</p>
+                            </div>
                         </motion.div>
                     </motion.div>
                 </motion.div>

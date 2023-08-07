50a700c5a87a173b199e7d219a47fafaf46b55fa
rush1dan <rushidan10@gmail.com>
2023-08-07 19:53:13 +0600

    For ultrathin phones

--- components/SkillCategory.tsx
+++ components/SkillCategory.tsx
@@ -13,7 +13,7 @@ type SkillCategoryProps = AnimateableComponentProps & {
 }
 
 export default function SkillCategory({ category, skillList, mediaQuery, iconBg = false, animationControls }: SkillCategoryProps) {
-    const iconSize: number = mediaQuery.lg ? 3.5 : (mediaQuery.md ? 3 : 2.5);
+    const iconSize: number = mediaQuery.lg ? 3.5 : (mediaQuery.md ? 3 : 2.25);
     const categoryHeaderSizeClass: string = mediaQuery.lg ? 'text-2xl' : (mediaQuery.md ? 'text-xl' : 'text-base');
 
     return (

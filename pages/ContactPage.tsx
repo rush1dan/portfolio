b6e058b5018a575eef4497afefd36b5784de3e7b
rush1dan <rushidan10@gmail.com>
2023-08-10 11:05:56 +0600

    Contact form configured for sending mails with formsubmit

--- pages/ContactPage.tsx
+++ pages/ContactPage.tsx
@@ -42,19 +42,20 @@ function ContactFormSection({ }: ContactFormProps) {
     return (
         <section className='absolute w-full top-1/2 -translate-y-1/2'>
             <div className="max-w-2xl mx-auto px-12">
-                <form action="#" className="space-y-8">
+                <form action="https://formsubmit.co/db65f7d1b09d1dfeaf8edd5e1257832d" method="POST" className="space-y-8">
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-300">Your email</label>
-                        <input type="email" id="email" className="text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white shadow-sm-light" placeholder="" required />
+                        <input type="email" id="email" name="email" className="text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white shadow-sm-light" placeholder="" required />
                     </div>
                     <div>
                         <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-300">Subject</label>
-                        <input type="text" id="subject" className="block p-3 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white shadow-sm-light" placeholder="" required />
+                        <input type="text" id="subject" name="subject" className="block p-3 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white shadow-sm-light" placeholder="" required />
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-400">Your message</label>
-                        <textarea id="message" rows={6} className="block p-2.5 w-full text-sm rounded-lg shadow-sm border bg-gray-700 border-gray-600 placeholder-gray-400 text-white" placeholder=""></textarea>
+                        <textarea id="message" name="message" rows={6} className="block p-2.5 w-full text-sm rounded-lg shadow-sm border bg-gray-700 border-gray-600 placeholder-gray-400 text-white" placeholder=""></textarea>
                     </div>
+                    <input type="hidden" name="_captcha" value="false" />
                     <button type="submit" className="py-3 px-5 text-sm font-medium text-center bg-slate-600 text-white rounded-lg sm:w-fit hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
                 </form>
             </div>

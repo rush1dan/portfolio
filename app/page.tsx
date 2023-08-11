a96efbad855b92e41c5a173a5f0453b6fbeaf991
rush1dan <rushidan10@gmail.com>
2023-08-11 19:15:45 +0600

    Provision for scroll to top button

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,18 @@ export default function Home() {
   return (
     <div className="bg-bgColor text-white h-screen 
     snap-y snap-mandatory scroll-smooth overflow-y-scroll overflow-x-hidden">
-      
+
       <head>
         <title>Rushidan's Portfolio</title>
       </head>
 
+      {/* Scroll To Top Button */}
+      <div className={`sticky top-[90vh] landscape:left-[93vw] left-[84vw] w-16 h-16 z-20 ${true ? 'hidden' : 'hidden'}`}>
+        <img src="icons/arrow-up.png" alt="Up Arrow" />
+      </div>
+
       {/* Header */}
-        <Header />
+      <Header />
 
       {/* Hero */}
       <section id="home">

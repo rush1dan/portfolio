3ef0e54db41bae8b0b1c299d489a032c2fe1c108
rush1dan <rushidan10@gmail.com>
2023-08-12 14:00:59 +0600

    Uparrow button img replaced with next/Image

--- pages/ContactPage.tsx
+++ pages/ContactPage.tsx
@@ -158,7 +158,7 @@ function Footer({ }: FooterProps) {
             </div>
             {/* Scroll To Top Button */}
             <Link href="#home" className={`absolute bottom-1/2 md:right-12 right-5 translate-y-1/2 w-16 h-16 z-20`}>
-                <img src="icons/arrow-up.png" alt="Up Arrow" />
+                <Image src="/icons/arrow-up.png" alt="Up Arrow" fill />
             </Link>
         </div>
     )

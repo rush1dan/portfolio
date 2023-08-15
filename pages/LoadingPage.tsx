d9106c1d6658739e2df6ed3de1c60f00f1ca0f04
rush1dan <rushidan10@gmail.com>
2023-08-15 11:22:33 +0600

    Loading bug resolved

--- pages/LoadingPage.tsx
+++ pages/LoadingPage.tsx
@@ -19,15 +19,14 @@ export default function LoadingPage({ loadNow = false, pageIndex, setPageToLoad,
     }, [isInView])
 
     return (
-        <div className='w-screen relative' ref={ref}>
+        <div className={`${!isInView && !loadNow ? 'min-h-screen' : ''} w-screen relative`} ref={ref}>
             {
                 (
                     !isInView && !loadNow ?
-                        <p className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl font-semibold'>
-                            <div className='xl:h-24 xl:w-24 md:h-20 md:w-20 h-16 w-16 relative'>
-                                <Image src="/icons/loading.gif" alt="profile picture" fill />
-                            </div>
-                        </p>
+                        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                        xl:h-24 xl:w-24 md:h-20 md:w-20 h-16 w-16'>
+                            <Image src="/icons/loading.gif" alt="profile picture" fill />
+                        </div>
                         : <Component />
                 )
             }

2767da8b1baa909cddad196ec9f59350408e3627
rush1dan <rushidan10@gmail.com>
2024-02-24 22:33:14 +0600

    Portfolio updated with ACI info

--- sections/AboutPage.tsx
+++ sections/AboutPage.tsx
@@ -16,14 +16,14 @@ export default function AboutPage({ isMobile, mediaQueries }: AboutPageProps) {
 }
 
 function AboutPageContent({ animationControls, isMobile, mediaQueries }: AnimateableComponentProps) {
-    const kolpoverseCard = <InfoCard
-        info={['Game Programmer', 'Kolpoverse Studios', 'November, 2022 - March, 2023']}
-        icons={['/icons/laptop.svg', '/icons/Kolpoverse.png', '/icons/calendar.svg']}
+    const aciCard = <InfoCard
+        info={['Software Engineer', 'ACI Limited', 'December, 2023 - Present']}
+        icons={['/icons/laptop.svg', '/icons/ACI.png', '/icons/calendar.svg']}
         rightAligned={false} />
 
-    const alphapotatoCard = <InfoCard
-        info={['Game Programmer', 'Alpha Potato', 'March, 2020 - November, 2022']}
-        icons={['/icons/laptop.svg', '/icons/AlphaPotato.png', '/icons/calendar.svg']}
+    const alphapotatoKolpoverseCard = <DualCompanyInfoCard
+        infos={[['Game Programmer'], ['Alpha Potato', '==>', 'Kolpoverse Studios'], ['March, 2020 - March, 2023']]}
+        icons={[['/icons/laptop.svg'], ['/icons/AlphaPotato.png', '', '/icons/Kolpoverse.png'], ['/icons/calendar.svg']]}
         rightAligned={false} />
 
     const buetCard = <InfoCard
@@ -60,7 +60,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                         I am a software engineer proficient in both frontend and backend technologies.
                         I have ample coding experience through my earlier professional ventures and personal projects.
                         I have working experience of game development, desktop app development and web app development, which makes me a well-rounded
-                        software developer. Equipped with an inquisitive mind and a knack for problem solving, I am always on the lookout for learning new things and growing myself 
+                        software developer. Equipped with an inquisitive mind and a knack for problem solving, I am always on the lookout for learning new things and growing myself
                         as an efficient and skilled software engineer.
                     </motion.p>
                 </section>
@@ -114,7 +114,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                                 animate={animationControls}
                                 transition={{ duration: 0.25, delay: 1 }}>
                                 {/* Description */}
-                                <motion.div className='absolute xl:left-32 h-12 -top-14 w-96'
+                                <motion.div className='absolute xl:left-32 h-12 -top-14 w-[32rem]'
                                     variants={{
                                         hidden: {
                                             x: 300,
@@ -128,7 +128,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                                     initial="hidden"
                                     animate={animationControls}
                                     transition={{ duration: 0.5, delay: 1.25 }}>
-                                    {kolpoverseCard}
+                                    {aciCard}
                                 </motion.div>
                             </motion.div>
                             {/* Education Middle */}
@@ -177,7 +177,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                                 animate={animationControls}
                                 transition={{ duration: 0.25, delay: 1 }}>
                                 {/* Description */}
-                                <motion.div className='absolute xl:left-32 h-12 -top-14 w-96'
+                                <motion.div className='absolute xl:left-32 h-12 -top-14 w-[32rem]'
                                     variants={{
                                         hidden: {
                                             x: 300,
@@ -191,7 +191,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                                     initial="hidden"
                                     animate={animationControls}
                                     transition={{ duration: 0.5, delay: 1.25 }}>
-                                    {alphapotatoCard}
+                                    {alphapotatoKolpoverseCard}
                                 </motion.div>
                             </motion.div>
                         </motion.div>
@@ -226,7 +226,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                                 initial="hidden"
                                 animate={animationControls}
                                 transition={{ duration: 0.5, delay: 0, ease: 'easeOut' }}>
-                                {kolpoverseCard}
+                                {aciCard}
                             </motion.div>
                             <motion.div className='md:w-[32rem] w-[19rem]'
                                 variants={{
@@ -242,7 +242,7 @@ function AboutPageContent({ animationControls, isMobile, mediaQueries }: Animate
                                 initial="hidden"
                                 animate={animationControls}
                                 transition={{ duration: 0.5, delay: 0.15, ease: 'easeOut' }}>
-                                {alphapotatoCard}
+                                {alphapotatoKolpoverseCard}
                             </motion.div>
                         </div>
                         {/* Education */}
@@ -293,6 +293,39 @@ function InfoCard({ info, icons, rightAligned }: InfoCardProps) {
                         <div key={index} className={`flex ${rightAligned ? 'flex-row-reverse' : 'flex-row'} items-center gap-x-2`}>
                             <div className='relative lg:w-6 lg:h-6 w-5 h-5 flex-none'><Image src={icons[index]} alt="" fill /></div>
                             <p className='lg:text-lg text-base font-medium'>{data}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+type DualInfoCardProps = {
+    infos: string[][],
+    icons: string[][],
+    rightAligned?: boolean
+}
+
+function DualCompanyInfoCard({ infos, icons, rightAligned }: DualInfoCardProps) {
+    return (
+        <div className='md:p-4 p-2 bg-zinc-700 rounded-lg border-2 border-zinc-400 shadow-stone-900 shadow-lg'>
+            {
+                infos.map((infoCluster, i) => {
+                    return (
+                        <div key={i} className={`flex ${rightAligned ? 'flex-row-reverse' : 'flex-row'} items-center gap-x-4`}>
+                            {
+                                infoCluster.map((info, j) => {
+                                    return (
+                                        icons[i][j] ? 
+                                        <div key={info} className={`flex ${rightAligned ? 'flex-row-reverse' : 'flex-row'} items-center gap-x-2`}>
+                                            <div className='relative lg:w-6 lg:h-6 w-5 h-5 flex-none'><Image src={icons[i][j]} alt="" fill /></div>
+                                            <p className='lg:text-lg text-base font-medium'>{info}</p>
+                                            </div> :
+                                            <p key={info} className='lg:text-lg text-base font-medium'>{info}</p>
+                                    )
+                                })
+                            }
                         </div>
                     )
                 })
